500b36f8759915d9d9c4ecfc4ce52050774be75e
Cristinamf <krismf98@gmail.com>
2018-11-27 08:56:53 +0100

    Primer commit

---
+++ Ejercicios arrays/src/Prueba/Prueba1.java
@@ -0,0 +1,22 @@
+package Prueba;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+//		String [] dias; //String declara que estamos haciendo String clase, no String constructor
+//		dias=new String [7];
+//		dias[0]="Lunes";
+//		dias[1]="Martes";
+//		dias[2]="Miercoles";
+//		dias[3]="Jueves";
+//		dias[4]="Viernes";
+//		dias[5]="Sabado";
+//		dias[6]="Domingo";
+	
+		String [] dias= {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+		for (int i=0; i<dias.length; i++) {
+			System.out.println(dias[i]);
+		}
+	}
+
+}
